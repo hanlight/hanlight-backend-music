7dbee71535e42a5cedfb772b9fa9f491cbd9e223
leensungzero <brianwoo119@naver.com>
2019-08-12 20:21:00 +0900

    Music model modeified && Music admin register

--- music/models.py
+++ music/models.py
@@ -2,10 +2,15 @@ from django.db import models
 
 
 class Album(models.Model):
-    name = models.CharField(max_length=150, unique=True)
+    id = models.PositiveIntegerField(primary_key=True)
+    name = models.CharField(max_length=150)
+    artist = models.CharField(max_length=100)
     image_url = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{name} - ({artist}, {id})".format(name=self.name, artist=self.artist, id=self.id)
 
 
 class Music(models.Model):
