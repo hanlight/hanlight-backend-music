567ca4d78a5ed7c482146020dd255e58c4b41681
leensungzero <brianwoo119@naver.com>
2019-08-13 19:13:47 +0900

    ADD: MusicViewSet permission setting

--- hanlight/settings/production.py
+++ hanlight/settings/production.py
@@ -151,3 +151,15 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+HANLIGHT_BASE_URL = 'https://test-backend.hanlight.kr/api/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
