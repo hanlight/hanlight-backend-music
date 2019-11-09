1f0d589a5ed07927cc6de545d29974b2113d4eb1
leensungzero <brianwoo119@naver.com>
2019-11-09 19:06:47 +0900

    MOD: Change crawling api target to FLO

--- music/crawling/music_search.py
+++ music/crawling/music_search.py
@@ -1,5 +1,4 @@
 import requests, random, json
-from bs4 import BeautifulSoup
 
 from music.models import Album
 
@@ -15,16 +14,10 @@ class MusicSearch:
         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8',
     ]
 
-    def __init__(self, param: str, type: str):
-        self.param = param
-        self.type = type
-        self.url = f"http://search.api.mnet.com/search/song?q={self.param}&domainCd="
-        if self.type == 'artist':
-            self.url += '1'
-        elif self.type == 'song':
-            self.url += '2'
-        elif self.type == 'album':
-            self.url += '3'
+    def __init__(self, keyword: str, type: str):
+        self.keyword = keyword  # 검색어
+        self.type = type  # 검색 타입 ( 전체, 곡, 앨범, 아티스트 )
+        self.url = f"https://www.music-flo.com/api/search/v2/search/integration?keyword={self.keyword}"
         self.user_agent = random.choice(self.USER_AGENT_LIST)
         self.headers = {
             'User-Agent': self.user_agent
@@ -34,9 +27,10 @@ class MusicSearch:
     def parse_data(self):
         res = requests.get(self.url, headers=self.headers)
         json_result = json.loads(res.text)
-        success = True if json_result['message'] == "성공" else False  # response status check
-        if not json_result['data'] == None:
-            self.music_data_list = json_result['data'][:10]
+        success = True if json_result['code'] == '2000000' else False  # response status check
+
+        if not json_result['data']['list'][1]['list']:
+            self.music_data_list = json_result['data']['list'][1]['list'][:10]
 
         return success, self.music_data_list
 
@@ -44,13 +38,16 @@ class MusicSearch:
         try:
             album = Album.objects.get(album_id=album_id)
         except Album.DoesNotExist:
-            url = f'http://www.mnet.com/album/{album_id}'
+            url = f'https://www.music-flo.com/api/meta/v1/album/{album_id}'
 
             res = requests.get(url, headers=self.headers)
-            html = res.text
-            soup = BeautifulSoup(html, 'html.parser')
+            json_result = json.loads(res.text)
+
             try:
-                album_image_url = soup.select_one('#big_img_profile')['src']
+                for img in json_result['data']['imgList']:
+                    if img['size'] == 1000:
+                        album_image_url = img['url']
+                        break
             except TypeError:
                 return None
 
@@ -69,17 +66,16 @@ class MusicSearch:
         if success:
             for music_data in music_data_list:
                 data = self.make_response_data(music_data)
-
                 search_result_list.append(data)
 
             return search_result_list
 
     def make_response_data(self, music_data):
-        song_name = music_data['songnm']
-        album_name = music_data['albumnm']
-        album_id = music_data['albumid']
+        song_name = music_data['name']
+        album_name = music_data['album']['title']
+        album_id = music_data['album']['id']
         album_id = int(album_id)
-        artist_name = music_data['artinfo'][0]['artistnm']
+        artist_name = music_data['artistList'][0]['name']
 
         album_image_url = self.get_or_save_album_image(album_id=album_id,
                                                        album_name=album_name,
